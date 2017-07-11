8a39cea15a0536abff238de5bf9903b8d1c0e184
biroa <pisze>
2017-07-11 15:35:51 +0200

    Naplóm oldalon listázás, új bejegyzés hozzáadása

--- src/app/new-happiness/new-happiness.component.ts
+++ src/app/new-happiness/new-happiness.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import { NotesService } from '../notes.service';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-new-happiness',
@@ -18,20 +20,23 @@ export class NewHappinessComponent implements OnInit {
     date: new Date()
   };
 
-  constructor() { }
+  myNote = new Note(new Date(), true, true, true);
+
+  constructor(private notesService: NotesService) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
-    this.note.gratitude1 = form.value.gratitude1;
-    this.note.gratitude2 = form.value.gratitude2;
-    this.note.gratitude3 = form.value.gratitude3;
-    this.note.gratitudes_done = (this.note.gratitude1 && this.note.gratitude2 && this.note.gratitude3) !== '';
-    this.note.exercise_done = form.value.exercise !== '';
-    this.note.meditation_done = form.value.meditation !== '';
-    this.note.kindness_done = form.value.kindness !== '';
-    this.note.date = new Date();
-    console.log(this.note);
+    this.myNote.setNoteData(
+      form.value.gratitude1,
+      form.value.gratitude2,
+      form.value.gratitude3,
+      (this.note.gratitude1 && this.note.gratitude2 && this.note.gratitude3) !== '',
+      form.value.exercise !== '',
+      form.value.meditation !== '',
+      form.value.kindness !== ''
+    );
+    this.notesService.editNote(this.myNote);
   }
 }
--- src/app/notes.service.ts
+++ src/app/notes.service.ts
@@ -2,29 +2,29 @@ import { Note } from './note';
 
 export class NotesService {
 
-  private dummyNotes = [];
+  private notes = [];
 
   constructor() {
     for (let i = 0; i < 21; i++) {
       const newDate = new Date();
       newDate.setDate(newDate.getDate() + i - 3);
       const newNote = new Note(newDate, true, true, true);
-      this.dummyNotes.push(newNote);
+      this.notes.push(newNote);
     }
 
-    this.dummyNotes[0].setNoteData(
+    this.notes[0].setNoteData(
       'Hogy fel tudtam kelni',
       'Hogy nem fogyott el a kávé',
       'Hogy felfértem a villamosra munkába menet',
       true, true, true);
 
-    this.dummyNotes[1].setNoteData(
+    this.notes[1].setNoteData(
       'Tudtam aludni a meleg ellenére',
       'Péntek van',
       '',
       false, true, true);
 
-    this.dummyNotes[2].setNoteData(
+    this.notes[2].setNoteData(
       'A pizzafutár 20 perc alatt ideért',
       'Nem merült le a mobilom útközben',
       'Anyukám húslevest főzőtt ebédre',
@@ -32,7 +32,13 @@ export class NotesService {
   }
 
   getNotes() {
-    return this.dummyNotes.slice();
+    return this.notes.slice();
+  }
+
+  editNote(editNote: Note) {
+    const today = new Date();
+    const editIndex = this.notes.findIndex(note => note.id.getDate() === today.getDate() && note.id.getMonth() === today.getMonth());
+    this.notes[editIndex] = editNote;
   }
 
 

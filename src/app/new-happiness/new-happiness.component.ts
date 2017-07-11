a39e35dd98a1a1bbd69605887f3ba8ec5655ee35
biroa <pisze>
2017-07-11 21:39:57 +0200

    new happiness hibajavítás_2

--- src/app/new-happiness/new-happiness.component.ts
+++ src/app/new-happiness/new-happiness.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {NgForm} from '@angular/forms';
 import { NotesService } from '../notes.service';
 import { Note } from '../note';
 import { DataStorageService } from '../data-storage.service';
@@ -30,24 +30,12 @@ export class NewHappinessComponent implements OnInit {
       form.value.kindness !== ''
     );
     this.notesService.editNote(this.myNote);
-  // onSubmit() {
-  //   this.myNote.setNoteData(
-  //     form.value.gratitude1,
-  //     form.value.gratitude2,
-  //     form.value.gratitude3,
-  //     (this.note.gratitude1 && this.note.gratitude2 && this.note.gratitude3) !== '',
-  //     form.value.exercise !== '',
-  //     form.value.meditation !== '',
-  //     form.value.kindness !== ''
-  //   );
-  //   this.notesService.editNote(this.myNote);
 
-    this.dataStorageService.storeNotes()
-      .subscribe(
-        (response: Response) => {
-          console.log(response);
-        }
-      );
-
+    // this.dataStorageService.storeNotes()
+    //   .subscribe(
+    //     (response: Response) => {
+    //       console.log(response);
+    //     }
+    //   );
   }
 }

85b71378d949eedbccadc603e09d781a038fbcaa
bramcsi <rmarci@gmail.com>
2017-07-11 09:42:29 +0200

    firebase initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Better Day app';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD633YnU0DJ4DA-V-IcbxCaLL2GAXXMjZY',
+      authDomain: 'betterday-94a8e.firebaseapp.com'
+    });
+  }
 }

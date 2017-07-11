1e0424a2dde3ca43a93b1fecf215d289cadc0165
radnoczim <radnoczi.marton@apertus.uni-nke.hu>
2017-07-11 14:07:04 +0200

    uj bejegyzes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import {DiaryPageComponent} from "./diary-page/diary-page.component";
 import {HappyDetailsCardComponent} from "./happy-details-card/happy-details-card.component";
 import {HomePageComponent} from "./home-page/home-page.component";
 import { SignupComponent } from 'app/signup/signup.component';
+import {NewHappinessComponent} from "./new-happiness/new-happiness.component";
 
 const appRoutes: Routes = [
-  { path: 'login', component: SignupComponent }
+  { path: 'login', component: SignupComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-
   { path: 'diary', component: DiaryPageComponent },
+  { path: 'new', component: NewHappinessComponent },
   { path: 'details', component: HappyDetailsCardComponent }
 ];
 

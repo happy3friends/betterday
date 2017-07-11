b6c4ef0c9dedf7a97dfacb86da8965ac565a43e2
radnoczim <radnoczi.marton@apertus.uni-nke.hu>
2017-07-11 13:05:16 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import {DiaryPageComponent} from "./diary-page/diary-page.component";
+import {HappyDetailsCardComponent} from "./happy-details-card/happy-details-card.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'diary', component: DiaryPageComponent },
+  { path: 'details', component: HappyDetailsCardComponent }
 ];
 
 @NgModule({

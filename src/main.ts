0a63e15b6660b98cca617c656b8f3f5cebc0426e
bramcsi <rmarci@gmail.com>
2017-07-13 14:35:41 +0200

    FB initiaization

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,14 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyD633YnU0DJ4DA-V-IcbxCaLL2GAXXMjZY',
+  authDomain: 'betterday-94a8e.firebaseapp.com',
+  databaseURL: 'https://betterday-94a8e.firebaseio.com',
+  projectId: 'betterday-94a8e',
+  storageBucket: '',
+  messagingSenderId: '372905412074'
+};
+firebase.initializeApp(config);
